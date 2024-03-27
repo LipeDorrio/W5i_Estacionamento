2d34cf4318bd1e0f0992e4991003b952f270f556
LipeDorrio <felipejohnjohn@gmail.com>
2024-03-27 20:37:07 -0300

    Primeiro commit

---
+++ categoria.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Categoria</title>
+</head>
+
+<body>
+    <h1>Nova categoria</h1>
+    <form>
+        <div class="mb=3">
+            <input type="text" name="">
+        </div>
+    </form>
+</body>
+
+</html>
