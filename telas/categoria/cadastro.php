5e0d38ba96766da8b6992c31df9d722448be56f9
LipeDorrio <felipejohnjohn@gmail.com>
2024-03-30 03:14:31 -0300

    Tela Lista Categoria, produção

--- telas/categoria/cadastro.php
+++ telas/categoria/cadastro.php
@@ -1,43 +1,4 @@
-<!doctype html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cadastro categoria</title>
-    <link href="../resources/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
-<body>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">W5i</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="cadastro.php">Cadastro</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="cadastroCategoria.php">Cadastro Categoria</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="relatorio.php">Relatório</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="pesquisa.php">Pesquisa</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-    <script src="../resources/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php require_once ("../../configuracao.php");?>
+<?php require_once($path_inc."/resources/topo.php"); ?>
+Lista categoria
+<?php require_once($path_inc."/resources/rodape.php"); ?>
--- telas/categoria/lista.php
+++ telas/categoria/lista.php
@@ -1,4 +1,83 @@
-<?php require_once ("../../configuracao.php");?>
-<?php require_once($path_inc."/resources/topo.php"); ?>
-Lista categoria
-<?php require_once($path_inc."/resources/rodape.php"); ?>
+<?php require_once ("../../configuracao.php"); ?>
+<?php require_once ($path_inc . "/resources/topo.php"); ?>
+<div class="container">
+    <div>
+        <div class="align">
+            <label for="formGroupExampleInput2" class="form-label">Filtro</label>
+            <input style="width :110px; height :40px" type="text" class="form-control" id="formGroupExampleInput2"
+                placeholder="Filtro">
+        </div>
+    </div>
+    <div class="button">
+        <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+</div>
+<button style="align-itens: right" class="btn btn-primary">Teste</button>
+<div class="table">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+<?php require_once ($path_inc . "/resources/rodape.php"); ?>
+
+<style>
+    .container {
+        width: 100vw;
+        height: 100px;
+        display: flex;
+        flex-direction: row;
+        justify-content: top;
+        align-items: top;
+        border: solid;
+        border-radius: 20px;
+        border-color: green;
+
+    }
+    .table{
+       display: column;
+       height: 100px;
+       height: 100px;
+    }
+    .align {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        justify-content: right;
+        align-items: right;
+    }
+
+    .button {
+        display: flex;
+        flex-direction: column;
+        justify-content: right;
+        align-items: ;
+    }
+</style>
