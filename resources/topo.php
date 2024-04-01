925b18d02ec9266ff6f3bf9d9adaf9608a70132c
LipeDorrio <felipejohnjohn@gmail.com>
2024-04-01 02:41:58 -0300

    Terminado

--- resources/topo.php
+++ resources/topo.php
@@ -7,12 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastro categoria</title>
     <link href="<?=$caminho?>/resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=$caminho?>/resources/css/style.css" rel="stylesheet">
 </head>
 
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">W5i</a>
+        <a class="navbar-brand" >W5i</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -23,13 +24,16 @@
                     <a class="nav-link active" aria-current="page" href="<?=$caminho ?>telas/index.php">Inicio</a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="<?=$caminho?>telas/movimentacao/lista.php">Estacionamento</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="<?=$caminho ?>telas/categoria/lista.php">Categoria</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?=$caminho ?>telas/veiculo/lista.php">Veiculo</a>
+                    <a class="nav-link" href="<?=$caminho ?>telas/veiculo/lista.php">Veículo</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="relatorio.php">Relatório</a>
+                    <a class="nav-link" href="<?=$caminho ?>telas/preco/lista.php">Tabela de Preço</a>
                 </li>
             </ul>
         </div>
